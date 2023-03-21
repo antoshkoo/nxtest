314062aa9d5454ccefbc7a9add3576b819390f14
Anton Yashin <antoshkoo@gmail.com>
2023-03-22 01:03:07 +0800

    init: sailing report on Bootstrap

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+import "../scss/styles.scss";
+// import * as bootstrap from "bootstrap";
+import { Tab, Card } from "bootstrap";
+
+const copyTextarea = document.getElementById("copyTextarea");
+const copyBtn = document.getElementById("copyBtn");
+copyBtn.addEventListener("click", () => {
+  copyTextarea.select();
+  document.execCommand("copy");
+});
+
+const formSailing = document.getElementById("formSailing");
+formSailing.addEventListener("change", () => {
+  let formEntities = {};
+  const formData = new FormData(formSailing);
+
+  for (const elem of formData.entries()) {
+    formEntities[elem[0]] = elem[1];
+  }
+  delete formEntities.copyTextarea;
+  copyTextarea.innerText = JSON.stringify(formEntities);
+});
